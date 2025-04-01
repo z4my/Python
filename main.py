5e19926719e719d30d55d954de523005a969b088
z4my <sumitfoujdarcse@gmail.com>
2025-04-01 23:27:51 +0530

    Add initial implementation of main.py with basic variable declarations and type demonstrations

---
+++ main.py
@@ -0,0 +1,35 @@
+print ("hello world")
+name = "Sumit"
+age = 20
+height = 5.8
+
+print(name)
+print(age)
+print(height)
+
+print("My name is: ", name)
+print("My age is: ", age)
+print("My height is: ", height)
+print("My name is: " + name)
+print("My age is: " + str(age))#to convert int to string 
+
+print(f"My name is {name}, and My age is {age} and My height is {height}")
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+print(type(str(age)))
+print(str(age))#to convert int to string
+print(type(int(height)))
+print(int(height))#to convert float to int
+print(type(float(age)))
+print(float(age))#to convert int to float
+print(type(bool(age)))
+print(bool(age))#to convert int to bool
+print(type(bool(name)))
+print(bool(name))#to convert string to bool
+print(type(bool(height)))
+print(bool(height))#to convert float to bool
+
+    

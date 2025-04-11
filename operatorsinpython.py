2285379bf785e799c053fa4a9beef8467096f96f
z4my <sumitfoujdarcse@gmail.com>
2025-04-12 02:59:04 +0530

    operatorsinpython.py

---
+++ operatorsinpython.py
@@ -0,0 +1,107 @@
+#Differet type of operators in python
+a= 5
+b= 2
+
+##Arithmetic operators
+# +, -, *, /, %, **, //
+print(a + b) # addition
+print(a - b) # subtraction
+print(a * b) # multiplication
+print(a / b) # division
+print(a % b) # modulus
+print(a ** b) # exponentiation
+print(a // b) # floor division
+
+
+##Comparison operators
+# ==, !=, >, <, >=, <=
+print(a == b) # equal to
+print(a != b) # not equal to
+print(a > b) # greater than
+print(a < b) # less than
+print(a >= b) # greater than or equal to
+print(a <= b) # less than or equal to
+
+##Assignment operators
+# =, +=, -=, *=, /=, %=, **=, //=
+# += : addition assignment
+#a+= 5 
+print("a :", a)
+# -= : subtraction assignment
+#a-= 5 
+print("a :", a)
+# *= : multiplication assignment
+#a*= 5 
+print("a :", a)
+# /= : division assignmen
+#a/= 5 
+print("a :", a)
+# %= : modulus assignment
+#a%= 5 
+print("a :", a)
+# **= : exponentiation assignment
+#a**= 5 
+print("a :", a)
+# //= : floor division assignment
+#a//= 5 
+print("a :", a)
+
+
+##Logical operators
+# and, or, not
+val1= False
+val2= False
+
+# and : logical and
+print(val1 and val2) # false
+print(val1 and True) # false
+# Both values should be true to return true
+
+# not : logical not
+print(not False) # true 
+print(not True) # false
+# It reverses the value of the operand
+
+# or : logical or
+print(val1 or val2) # false
+print(val1 or True) # true
+# Atleast one value should be true to return true
+
+
+##Identity operators
+
+# is, is not
+
+# is : identity
+
+# is not : not identity
+
+
+##Membership operators
+
+# in, not in
+
+# in : membership
+
+# not in : not membership
+
+
+##Bitwise operators
+# &, |, ^, ~, <<, >>
+
+# & : bitwise and
+
+# | : bitwise or
+
+# ^ : bitwise xor
+
+# ~ : bitwise not
+
+# << : left shift
+
+# >> : right shift
+
+
+##Ternary operator
+
+# (condition) ? (true value) : (false value)

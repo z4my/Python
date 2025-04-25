142a8c237f1a8175e898aed185d529f22b615678
z4my <sumitfoujdarcse@gmail.com>
2025-04-25 16:05:22 +0530

    Add input_in_python.py for user input demonstration with type conversions

---
+++ input_in_python.py
@@ -0,0 +1,6 @@
+name=input("enter your name:")
+age=int(input("enter your age:"))#converting str to int
+marks=float(input("enter your marks:"))#converting str to float
+print("Hello",name)#result for input() is always a str
+print("Your age is:",age)
+print("Your marks are:",marks)

3016a62199f6c62e82147caa9d86a644a3b6ffd0
z4my <sumitfoujdarcse@gmail.com>
2025-04-25 21:38:16 +0530

    Add initial implementations for data types, operators, user input, and practice problems

---
+++ Practice1.py
@@ -0,0 +1,39 @@
+## Q.1 Write a program to input 2 numbers and print their sum.
+
+#num1=int(input("Enter first number:"))
+#num2=int(input("Enter second number:"))
+#print("Sum=",num1+num2)
+
+####OR####
+#sum=num1+num2
+#print("Sum of",num1,"and",num2,"is:",sum)
+
+## Q.2 Write a program to input side of a sqquare and print its area.
+
+#side=int(input("Enter side of square:"))
+#print("area=",side*side)
+
+####OR####
+#print("area=",side**2)
+
+####OR####
+#Area=side*side
+#print("Area of square with side",Side,"is:",Area)
+
+## Q.3 Write a program to input 2 floating point numbers and print their average.
+
+#a=float(input("Enter first number:"))
+#b=float(input("Enter second number:"))
+#print("Average=",(a+b)/2)
+
+## Q.4 Write a program to input 2 int numbers, a and b. Print True if a is greater than or equal to b. If not print Flase.
+
+#a=int(input("Enter first number:"))
+#b=int(input("Enter second number:"))
+#print(a>=b)
+
+##OR####
+#if a>=b:
+#   print("True")
+#else:
+#   print("False")
